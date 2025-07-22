fa060c9f1e36b3759fe6076eb2677c14714f5183
NikM3 <nikolas.a.melendez@gmail.com>
2025-07-21 23:46:00 -0400

    Controller stubs with assumptions for service functions

---
+++ server/src/main/java/wotc/controller/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package wotc.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception ex) throws Exception {
+        return ErrorResponse.build("Something unexpected happened! Unhandled error.");
+    }
+}
