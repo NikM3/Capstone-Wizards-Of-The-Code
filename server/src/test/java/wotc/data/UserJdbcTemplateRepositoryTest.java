256505abfb88e9f70e292321746dbe8b81655b1c
Dev-OriolCC <cesaroriolcortezz@gmail.com>
2025-07-22 10:37:16 -0500

    Added Security authentication

--- server/src/test/java/wotc/data/UserJdbcTemplateRepositoryTest.java
+++ server/src/test/java/wotc/data/UserJdbcTemplateRepositoryTest.java
@@ -4,7 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.test.context.ActiveProfiles;
 import wotc.models.User;
+import wotc.security.JwtAuthenticationFilter;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,18 +27,18 @@ class UserJdbcTemplateRepositoryTest {
 
     @Test
     void shouldFindByUsername() {
-        User user = repository.findByUsername("test user2");
-        assertEquals(2, user.getUserId());
-        assertEquals("test user2", user.getUsername());
-        assertEquals("user2@test.com", user.getEmail());
+        User user = repository.findByUsername("admin");
+        assertEquals(3, user.getUserId());
+        assertEquals("admin", user.getActualUsername());
+        assertEquals("admin@mail.com", user.getEmail());
     }
 
     @Test
     void shouldFindByEmail() {
-        User user = repository.findByEmail("user2@test.com");
-        assertEquals(2, user.getUserId());
-        assertEquals("test user2", user.getUsername());
-        assertEquals("user2@test.com", user.getEmail());
+        User user = repository.findByEmail("admin@mail.com");
+        assertEquals(3, user.getUserId());
+        assertEquals("admin", user.getActualUsername());
+        assertEquals("admin@mail.com", user.getEmail());
     }
 
     @Test
@@ -44,13 +47,13 @@ class UserJdbcTemplateRepositoryTest {
         User actual = repository.add(user);
         assertNotNull(actual);
         assertEquals(NEXT_ID, actual.getUserId());
-        assertEquals("walter", actual.getUsername());
+        assertEquals("walter", actual.getActualUsername());
         assertEquals("walterwhite@mail.com", actual.getEmail());
     }
 
     @Test
     void shouldUpdate() {
-        User user = new User(NEXT_ID, "george", "walterwhite@mail.com", "5608-8007-b13d-5f6eacc2f176", false,null);
+        User user = new User(1, "george", "george@mail.com", "5608-8007-b13d-5f6eacc2f176", false,null);
         assertTrue(repository.update(user));
         user.setUserId(25);
         assertFalse(repository.update(user));
