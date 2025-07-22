33767f809d7d557184326ba50001a3776a2209d5
Taylor Springob <taylor.springob@gmail.com>
2025-07-22 15:00:21 -0400

    CollectionController CollectedCardController base implementation

--- server/src/main/java/wotc/controller/CollectionController.java
+++ server/src/main/java/wotc/controller/CollectionController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import wotc.domain.CollectionService;
 import wotc.domain.Result;
+import wotc.domain.ResultType;
 import wotc.models.CollectedCard;
 import wotc.models.Collection;
 
@@ -25,50 +26,40 @@ public class CollectionController {
         return service.findCollectionByUser(userId);
     }
 
-    @GetMapping("/cards/{collectionId}")
-    public List<CollectedCard> findCollectedCards(@PathVariable int collectionId) {
-        // return service.findCollectedCardsByCollection(collectionId);
-        return null;
-    }
-
-    @PostMapping("/cards/{collectionId}")
-    public ResponseEntity<Object> addCollectedCard(@RequestBody CollectedCard cc, @PathVariable int collectionId) {
-        //Result<CollectedCard> result = service.addCollectedCard(cc);
-        System.out.println("Are we getting into the method at all?");
-        Result<CollectedCard> result = new Result<>();
+    @PostMapping
+    public ResponseEntity<Object> add(@RequestBody Collection collection) {
+        Result<Collection> result = service.add(collection);
 
         if (result.isSuccess()) {
             return new ResponseEntity<>(result.getPayload(), HttpStatus.CREATED);
         }
-
         return ErrorResponse.build(result);
     }
 
-    @PutMapping("/cards/card/{collectedCardId}")
-    public ResponseEntity<Object> editCollectedCard(@RequestBody CollectedCard cc, @PathVariable int collectedCardId) {
-        if (collectedCardId != cc.getCollectedCardId()) {
+    @PutMapping("/{collectionId}")
+    public ResponseEntity<Object> update(@PathVariable int collectionId, @RequestBody Collection collection) {
+        if (collectionId != collection.getCollectionId()) {
             return new ResponseEntity<>(HttpStatus.CONFLICT);
         }
 
-        // Result<CollectedCard> result = service.editCollectedCard(cc);
-        Result<CollectedCard> result = new Result<>();
-
+        Result<Collection> result = service.editCollection(collection);
         if (result.isSuccess()) {
-            return new ResponseEntity<>(result.getPayload(), HttpStatus.NO_CONTENT);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
-
         return ErrorResponse.build(result);
     }
 
-    @DeleteMapping("/cards/card/{collectedCardId}")
-    public ResponseEntity<Object> deleteCollectedCard(@PathVariable int collectedCardId) {
-        // Result<CollectedCard> result = service.deleteCollectedCardById(collectedCardId);
-        Result<CollectedCard> result = new Result<>();
+    @DeleteMapping("/{collectionId}")
+    public ResponseEntity<Void> delete(@PathVariable int collectionId) {
+        Result<Collection> result = service.deleteById(collectionId);
 
-        if (result.isSuccess()) {
-            return new ResponseEntity<>(result.getPayload(), HttpStatus.NO_CONTENT);
+        if (result.getType() == ResultType.NOT_FOUND) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
-        return ErrorResponse.build(result);
+        if (result.getType() == ResultType.INVALID) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
