9597a12de11b5a68779bdc0f2057c167d699871c
NikM3 <nikolas.a.melendez@gmail.com>
2025-07-22 21:20:54 -0400

    Replaced depreciated MockBeans and improved handling in GlobalExceptionHandler

--- server/src/test/java/wotc/domain/CollectionServiceTest.java
+++ server/src/test/java/wotc/domain/CollectionServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import wotc.data.CollectionRepository;
 import wotc.models.Collection;
 
@@ -15,7 +16,7 @@ class CollectionServiceTest {
     @Autowired
     CollectionService service;
 
-    @MockBean
+    @MockitoBean
     CollectionRepository repository;
 
     @Test
